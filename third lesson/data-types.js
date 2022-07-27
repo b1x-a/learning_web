e1e0f65716a19718a1dbd4fb4d9556227618d2bb
Pavelbev <98077341+b1x-a@users.noreply.github.com>
2022-07-27 17:00:42 +0300

    initital third lesson

---
+++ third lesson/data-types.js
@@ -0,0 +1,27 @@
+const firstName = 'Pavel';
+console.log(typeof firstName);
+
+const age = 23;
+console.log(typeof age);
+
+const thisBooleanType = true;
+console.log(typeof thisBooleanType);
+
+const symbolType = Symbol('specially');
+console.log(typeof symbolType);
+
+let valueUndefined;
+console.log(typeof valueUndefined);
+
+const valueNull = null;
+console.log(valueNull);
+
+const userInfo = {
+   firstName: 'Pavel',
+   age: 23,
+   favoriteAnimals: 'cat'
+};
+console.log(typeof userInfo);
+
+const valueN = BigInt(21);
+console.log(typeof valueN);
